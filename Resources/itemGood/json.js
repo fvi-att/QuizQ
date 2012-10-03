87b220b1ccc559c7607d8b4596628789804e0ad4
fvi-att <jshimizujp@gmail.com>
2012-10-03 14:15:51 +0900

    merge to new one 201210031215

--- Resources/itemGood/json.js
+++ Resources/itemGood/json.js
@@ -6,8 +6,19 @@
  * CREATED @ 20121002
  *
  */
-
-exports.env = function(from) {
+exports.toString = function(data){
+	var ret_word = '{';
+	if(data.money)
+		ret_word += 'money:'+data.money;
+	
+	if(data.crop)
+		ret_word += ',crop:'+data.crops;
+	
+	ret_word += '}'
+	
+	return ret_word;
+}
+exports.eval = function(from) {
 	//JSON文字列からデータを形成
 	try {
 		return eval(from);
