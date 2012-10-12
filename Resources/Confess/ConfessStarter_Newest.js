23f496d44a29b3a141a4f8a06fc7829cf971ac68
fvi-att <jshimizujp@gmail.com>
2012-10-12 12:38:40 +0900

    add show system

---
+++ Resources/Confess/ConfessStarter_Newest.js
@@ -0,0 +1,45 @@
+/**
+ * @author fvi@
+ *
+ * created@ 2012 08 09
+ *
+ * recreated @2012112
+ * 
+ */
+
+exports.FlowdownloadStart = function(post_id) {
+	
+	var actInd = Titanium.UI.createActivityIndicator({
+		bottom : 10,
+		height : 100,
+		width : 100,
+		message : 'つぶやきを集めています',
+		font : {
+			fontFamily : 'Helvetica Neue',
+			fontSize : 15,
+			fontWeight : 'bold'
+		}
+		// style:Titanium.UI.iPhone.ActivityIndicatorStyle.PLAIN
+	});
+
+	actInd.show();
+
+	var Cloud = require('ti.cloud');
+	Cloud.Posts.show({
+		post_id : post_id
+	}, function(e) {
+		if (e.success) {
+			var post = e.posts[0];
+			actInd.hide();
+			
+			 var flowWin = new require('/ui/common/ConfessWindow/FlowView')(e.posts);
+			
+			alert('Success:\\n' + 'id: ' + post.id + '\\n' + 'title: ' + post.title + '\\n' + 'content: ' + post.content + '\\n' + 'updated_at: ' + post.updated_at);
+		} else {
+			alert('Error:\\n' + ((e.error && e.message) || JSON.stringify(e)));
+			
+			actInd.hide();
+		}
+	});
+
+}
---
+++ Resources/ui/common/ConfessWindow/FlowView.js
@@ -0,0 +1,78 @@
+/**
+ * @author fvi
+ *
+ * created@ 2012 07 19
+ *
+ */
+
+function ProjectList(download) {
+	/*
+	 * e.posts[0]
+	 * 
+	 * 
+	 */
+	height = Ti.Platform.displayCaps.platformHeight, width = Ti.Platform.displayCaps.platformWidth;
+
+
+	var projectList = [];
+	/*
+	//plus section
+	projectList.push(Titanium.UI.createTableViewRow({
+		width : Titanium.UI.FILL,
+		height : height / 15,
+		focusable : false,
+		backgroundImage : '/images/Table/favorite_1.png'
+	}));
+	projectList.push(Titanium.UI.createTableViewRow({
+		width : Titanium.UI.FILL,
+		height : height / 15,
+		focusable : false,
+		backgroundImage : '/images/Table/section_1.png'
+	}));
+	*/
+	// Create a TableView.
+	var aTableView = Ti.UI.createTableView({
+		data : projectList,
+		showVerticalScrollIndicator : true,
+		top : height * 0.05,
+		width : Titanium.UI.FILL,
+		height : height * 0.7
+	});
+	var STUB_row = require('/ui/common/ConfessWindow/FlowRow').createRowObject('',download[0].title, 0,null);
+	STUB_row.row.setHasChild(false);
+	
+	aTableView.appendRow(STUB_row.row);
+	
+	
+	aTableView.addEventListener('click',function(e){
+		alert(JSON.stringify(e));
+	})
+
+
+	var view = Titanium.UI.createWindow({
+		title : L('collection'),
+		navBarHidden : true, //タイトルバーを隠す
+		//backgroundImage : '/images/opening/old_paper.jpg',
+		backgroundColor:'rgb(255,235,205)',//ivory color
+
+		exitOnClose : false,
+		fullscreen : true,
+		orientationModes : [Titanium.UI.PORTRAIT]
+
+	});
+
+
+	//	view.add(searchBar);
+	view.add(aTableView);
+
+
+
+
+	view.open();
+
+	return view;
+
+}
+
+module.exports = ProjectList;
+
