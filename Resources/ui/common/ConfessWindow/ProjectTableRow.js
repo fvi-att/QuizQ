21846db86846b56abcde2c6cc2e01cbe9b478c91
= <=>
2012-11-30 18:48:41 +0900

    remove row className try to another solution 201211301848

--- Resources/ui/common/ConfessWindow/ProjectTableRow.js
+++ Resources/ui/common/ConfessWindow/ProjectTableRow.js
@@ -24,7 +24,7 @@ exports.createRowObject = function(image_path, title, side, photo, id) {
 		backgroundImage : '/images/transparent.png',
 		hasChild : true,
 		height : height * 0.2 + height * 0.05 * lf_count + hasPhoto * height * 0.30,
-		className : 'todo_row'+title+id,
+		//className : 'todo_row'+title+id,
 		id : id,
 
 	});
