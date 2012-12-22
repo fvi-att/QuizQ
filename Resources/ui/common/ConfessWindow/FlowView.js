4ee262b4ddacc6a2a8de21fa7d1c80bbb39e90c0
fvi-att <jshimizujp@gmail.com>
2012-12-23 01:08:18 +0900

    add bunner admob module 201212220108

--- Resources/ui/common/ConfessWindow/FlowView.js
+++ Resources/ui/common/ConfessWindow/FlowView.js
@@ -6,6 +6,7 @@
  */
 
 function FlowWindow(download) {
+	height = Ti.Platform.displayCaps.platformHeight, width = Ti.Platform.displayCaps.platformWidth;
 	var win = require('/ui/common/CommonFlowTableWindow')()
 	var flowTableView = win.table;
 	//テーブルがセットになっているのでそこに付け足していく感じで書いていく。
@@ -15,6 +16,29 @@ function FlowWindow(download) {
 	for(cnt = 0;cnt < row_array.length;cnt++){
 		flowTableView.appendRow(row_array[cnt].row);
 	}
+	
+	// require AdMob
+var Admob = require('ti.admob');
+
+// then create an adMob view
+var adMobView = Admob.createView({
+    publisherId:"a150bf49d1e3f16",
+    testing:false, // default is false
+    //top: 10, //optional
+    //left: 0, // optional
+    //right: 0, // optional
+    bottom: height * 0.1, // optional
+    adBackgroundColor:"FF8855", // optional
+    backgroundColorTop: "738000", //optional - Gradient background color at top
+    borderColor: "#000000", // optional - Border color
+    textColor: "#000000", // optional - Text color
+    urlColor: "#00FF00", // optional - URL color
+    linkColor: "#0000FF" //optional -  Link text color
+    //primaryTextColor: "blue", // deprecated -- now maps to textColor
+    //secondaryTextColor: "green" // deprecated -- now maps to linkColor
+    
+});
+win.add(adMobView);
 }
 
 module.exports = FlowWindow;
